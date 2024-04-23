accc0f8d8966a35c0f388be0faceb350301f37d8
Ricardo Vaz <rvaz@imaginarycloud.com>
2024-04-23 15:25:28 +0100

    Configured ESLint and Prettier

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,19 +2,16 @@ import pgPromise from 'pg-promise';
 
 // Initialize pgPromise instance
 const pgp = pgPromise({
-    capSQL: true
+  capSQL: true
 });
 
 // Create a new dbConnection
 const db_connection = pgp({
-    host: process.env.DB_HOST,
-    port: parseInt(<string>process.env.DB_PORT, 10),
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+  host: process.env.DB_HOST,
+  port: parseInt(<string>process.env.DB_PORT, 10),
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 });
 
-export default {
-    db: db_connection,
-    helpers: pgp.helpers 
-};
+export default { db: db_connection, helpers: pgp.helpers };
--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,11 @@ app.use(helmet());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello world!');
+  res.status(200).send('Hello world!');
 });
 
 app.listen(parseInt(process.env.EXPRESS_SERVER_PORT!, 10), () => {
-    console.log(`Server running and listening on port ${process.env.EXPRESS_SERVER_PORT}`);
+  console.log(
+    `Server running and listening on port ${process.env.EXPRESS_SERVER_PORT}`
+  );
 });
